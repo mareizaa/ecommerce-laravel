2485861424a3e0f37c7c915ad35009092e296a7e
mareizaa <marce.3210@gmail.com>
2022-01-26 10:38:12 -0500

    php-cs-fix

--- database/seeders/users/UsersSeeder.php
+++ database/seeders/users/UsersSeeder.php
@@ -23,16 +23,16 @@ class UsersSeeder extends Seeder
                 'status' => '1',
                 'email_verified_at' => now(),
             ]
-            );
-            DB::table('users')->insert(
-                [
-                    'name' => 'Usuario',
-                    'email' => 'user@gmail.com',
-                    'password' => bcrypt('12345678'),
-                    'role' => 'client',
-                    'status' => '0',
-                    'email_verified_at' => now(),
-                ]
-                );
+        );
+        DB::table('users')->insert(
+            [
+                'name' => 'Usuario',
+                'email' => 'user@gmail.com',
+                'password' => bcrypt('12345678'),
+                'role' => 'client',
+                'status' => '0',
+                'email_verified_at' => now(),
+            ]
+        );
     }
 }
