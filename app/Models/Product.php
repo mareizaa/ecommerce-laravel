e08c53ff63d7c5479b082fcbfa561808b4a0ac17
mareizaa <marce.3210@gmail.com>
2022-02-06 23:35:12 -0500

    Update model, factory and migrations class product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
+use App\Models\Image;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
     use HasFactory;
+
+    public function image()
+    {
+        return $this->hasMany(Image::class);
+    }
 }
