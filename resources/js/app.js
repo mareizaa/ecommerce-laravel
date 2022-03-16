96155564415f2d2580c1a937e53b7fed91bc2cac
mareizaa <marce.3210@gmail.com>
2022-03-16 17:08:49 -0500

    Instalation Vue3

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,11 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+import { createApp } from 'vue'
+import ShoppingCart from './components/shoppingCart/ShoppingCart'
+
+const app = createApp({ })
+
+app.component('cart', ShoppingCart)
+app.mount('#app')
---
+++ resources/js/components/shoppingCart/ShoppingCart.vue
@@ -0,0 +1,18 @@
+<template>
+
+<slot>
+    <div>
+        <p>Shopping Cart</p>
+    </div>
+</slot>
+
+</template>
+<script>
+
+export default ({
+    data() {
+        
+    },
+})
+
+</script>
