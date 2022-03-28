6f50876e87e32bf9aaceb8e4657a56d617c324ab
mareizaa <marce.3210@gmail.com>
2022-03-27 19:15:18 -0500

    Add in view welcome button add products

---
+++ config/webcheckout.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'baseUrl' => env('WEBCHECKOUT_URL', null),
+    'login' => env('WEBCHECKOUT_LOGIN', null),
+    'tranKey' => env('WEBCHECKOUT_TRANKEY', null)
+];
---
+++ database/migrations/2022_03_27_172536_create_orders.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrders extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned();
+            $table->integer('total');
+            $table->string('currency')->default("COP");
+            $table->enum('state', ['approved', 'pending', 'rejected'])->default('pending');
+            $table->integer('session_id');
+            $table->string('session_url');
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
