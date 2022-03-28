68deb97969fd3cadfcf591ee21f7210f6c0a3d59
mareizaa <marce.3210@gmail.com>
2022-03-27 19:16:46 -0500

    Create Model for orders and jquery installation

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,10 +5,3 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
-
-import { createApp } from 'vue'
-import ShoppingCart from "./components/shoppingCart/ShoppingCart";
-
-const app = createApp({})
-app.component('shopping-cart', ShoppingCart)
-app.mount('#app')
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,16 +11,24 @@
                         </div>
                         <div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
                             @foreach ($products as $product)
-                                <a href="{{ route('products.show', $product->id) }}" class="group">
+                                <div>
                                     <div class="w-full h-80 aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-                                        @foreach ($product->image as $image)
-                                            <img src="{{ asset('storage/images/'.$product->id.'/'.$image->image_name) }}" alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." class="w-full h-full object-center object-cover group-hover:opacity-75">
-                                        @endforeach
+                                        <a href="{{ route('products.show', $product->id) }}" class="group">
+                                            @foreach ($product->image as $image)
+                                                <img src="{{ asset('storage/images/'.$product->id.'/'.$image->image_name) }}" alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." class="w-full h-full object-center object-cover group-hover:opacity-75">
+                                            @endforeach
+                                        </a>
                                     </div>
-                                    <h3 class="mt-4 text-sm text-gray-700">{{ $product->name }}</h3>
-                                    <p class="mt-1 text-lg font-medium text-gray-900">${{ $product->price }}</p>
-                                </a>
-                                <button x-on:click="pushProducts({{$product}})"> Agregar </button>
+                                    <div class="flex justify-between">
+                                        <div>
+                                            <h3 class="mt-4 text-sm text-gray-700">{{ $product->name }}</h3>
+                                            <p class="mt-1 text-lg font-medium text-gray-900">${{ $product->price }}</p>
+                                        </div>
+                                        <div>
+                                            <button class="mt-4 bg-yellow-300 opacity-75 hover:opacity-100 text-yellow-900 hover:text-gray-900 rounded-full px-10 py-2 font-semibold" @click="count++"><i class="mdi mdi-cart -ml-2 mr-2"></i> BUY NOW</button>
+                                        </div>
+                                    </div>
+                                </div>
                             @endforeach
                         </div>
                         <div class="mt-4">
